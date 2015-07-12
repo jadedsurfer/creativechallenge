46516f96c244e234b200470d597fb2c766601413
Todd Bashor <jadedsurfer@gmail.com>
2015-07-11 17:21:46 -0700

    Add logging to server.js

--- server/server.js
+++ server/server.js
@@ -1,33 +1,29 @@
-//var loopback = require('loopback');
-//var boot = require('loopback-boot');
-////var finishAuthConfig = require('./post-boot/auth');
-//
-//var app = module.exports = loopback();
-//
-//app.start = function appStart() {
-//  // start the web server
-//  return app.listen(function appListen() {
-//    app.emit('started');
-//    console.log('Web server listening at: %s', app.get('url'));
-//  });
-//};
-//
-//// Bootstrap the application, configure models, datasources and middleware.
-//// Sub-apps like REST API are mounted via boot scripts.
-//boot(app, __dirname, function startApp(err, sys){
-//
-//// start the server if `$ node server.js`
-//  if (require.main === module) {
-//    //finishAuthConfig(app);
-//    app.start();
-//  }
-//});
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+//var finishAuthConfig = require('./post-boot/auth');
 
-var http = require('http');
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var app = module.exports = loopback();
+
+app.start = function appStart() {
+  // start the web server
+  console.log('start');
+  return app.listen(function appListen() {
+    app.emit('started');
+    console.log('Web server listening at: %s', app.get('url'));
+  });
+};
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+console.log('boot');
+boot(app, __dirname, function startApp(err, sys){
+
+// start the server if `$ node server.js`
+  if (require.main === module) {
+    //finishAuthConfig(app);
+    app.start();
+  }
+});
+
 
 

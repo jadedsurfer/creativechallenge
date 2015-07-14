d28efb8931c10c7743489c661f57195eec3b5ddc
Todd Bashor <jadedsurfer@gmail.com>
2015-07-14 13:51:15 -0700

    Modify the plugin-loader and config to use the newly refactored plugins

--- server/boot/plugin-loader.js
+++ server/boot/plugin-loader.js
@@ -8,15 +8,29 @@ module.exports = function loadApps(server, cb) {
   // Fill in plugin information from package.json
   var projectRoot = path.join(__dirname, '../../');
   var plugins = architect.resolveConfig(server.settings.plugins, projectRoot);
+  var registeredServices = [];
 
   // Inject server so it is available to the plugins
   var api = {
     server: server,
-    Router: server.loopback.Router
+    Router: server.loopback.Router,
+    models: server.models,
+    datasources: server.datasources,
+    cookieParserMiddleware: server.loopback.cookieParser,
+    sessionMiddleware: server.loopback.session,
+    staticMiddleware: server.loopback.static
   };
   plugins.push({
     consumes: [],
-    provides: ['server', 'Router'],
+    provides: [
+      'server',
+      'Router',
+      'models',
+      'datasources',
+      'cookieParserMiddleware',
+      'sessionMiddleware',
+      'staticMiddleware'
+    ],
     setup: function(options, imports, register) {
       register(null, api);
     }
@@ -28,13 +42,16 @@ module.exports = function loadApps(server, cb) {
   // Handle when a new service is registered
   system.on('service', function(service){
     //console.log(service);
+    registeredServices.push(service);
   });
 
   // Handle any errors
   system.on('error', function(err){
     if (err) {
-      console.error('problem creating the apps');
-      console.error(err);
+      console.error('problem adding plugins to app');
+      console.log('successfully registered these plugins: %O',
+        registeredServices );
+      //console.error(err);
       return cb(err);
     }
   });
@@ -43,6 +60,11 @@ module.exports = function loadApps(server, cb) {
   system.on('ready', function(sys){
 //    console.log(sys);
 //    console.log(sys.services.server.loopback().middleware);
+    process.nextTick(function(){
+      console.log('successfully registered these plugins',
+        registeredServices );
+    });
+    server._plugins = sys;
     return cb(null, sys);
   });
 

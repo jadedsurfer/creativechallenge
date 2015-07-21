01a573467bc7a12c49b964f184b47404b27ad303
Todd Bashor <jadedsurfer@gmail.com>
2015-07-21 16:07:58 -0700

    #5 - Add ability to vote when signed in but haven't previously voted on the submission

--- ionic/www/js/submission/submission.controller.js
+++ ionic/www/js/submission/submission.controller.js
@@ -1,3 +1,33 @@
-module.exports = function($scope, $stateParams, Submission) {
-    $scope.submission = Submission.get({id: $stateParams.submissionId});
+module.exports = function($scope, $stateParams, Submission, AppAuth, Vote) {
+    //$scope.submission = Submission.get({id: $stateParams.submissionId});
+
+  $scope.hideVoting = ($scope.$parent.currentUser.id !== null) ? false : true;
+
+  $scope.addVote = function(){
+    Vote.create({
+      userId: $scope.$parent.currentUser.id,
+      submissionId: $scope.submission.id
+    }).$promise
+      .then(function(data){
+        $scope.submission.votes.push(data);
+        $scope.hideVoting=true;
+      }, function(err){
+        console.log(err);
+      });
+  };
+
+  var i = 0;
+  var votes = $scope.submission.votes;
+
+  for(i; i < votes.length; i++){
+    var currentVote = votes[i];
+    var currentUser = $scope.$parent.currentUser;
+    if (currentVote.userId === currentUser.id) {
+      $scope.hideVoting = true;
+      break;
+    }
+  }
+
+
 };
+

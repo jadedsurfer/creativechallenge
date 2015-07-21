3abd52b729d0c13d2efc13413048cc5fc3e69633
Todd Bashor <jadedsurfer@gmail.com>
2015-07-21 16:08:25 -0700

    #7 - Add ability to view number of votes for a submission

--- ionic/www/js/submission/submissions.controller.js
+++ ionic/www/js/submission/submissions.controller.js
@@ -1,20 +1,31 @@
 module.exports = function($scope, $stateParams, Submission, User, AppAuth){
 
-    var challengeId = $stateParams.id;
+  var challengeId = $stateParams.id;
 
-    if (AppAuth.currentUser) {
-      $scope.userPhotoUrl =
-        AppAuth.currentUser.profiles[1].profile.photos[0].value;
+  AppAuth.ensureHasCurrentUser(function(user) {
+    if (user && user.email && user.id) {
+      $scope.currentUser =  user;
+      if (user.profiles[1].profile.photos[0]) {
+        $scope.userPhotoUrl =
+          user.profiles[1].profile.photos[0].value;
+      } else {
+        $scope.userPhotoUrl = '';
+      }
+    } else {
+      $scope.currentUser = {anonymous: true, id: null};
+      $scope.hideVoting = true;
     }
+  });
 
-
-    Submission.find({ filter: {
-      where: {challengeId: challengeId},
-      include: ['votes', 'challenge']
-    }}).$promise
-      .then(function(submissions){
-        console.log(submissions);
-        $scope.challenge = submissions[0].challenge;
-        $scope.submissions = submissions;
-    });
+  Submission.find({ filter: {
+    where: {challengeId: challengeId},
+    include: ['votes', 'challenge']
+  }}).$promise
+    .then(function(submissions){
+      //console.log(submissions);
+      $scope.challenge = submissions[0].challenge;
+      $scope.submissions = submissions;
+  }, function(err){
+    console.log(err);
+  });
 };

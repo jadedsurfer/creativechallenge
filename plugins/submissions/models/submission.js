4f8ce77a26c3911f6130e33282a6f69d0754484d
Todd Bashor <jadedsurfer@gmail.com>
2015-07-22 12:52:25 -0700

    #29 - Convert submission.url to AWS signed url prior to response

--- plugins/submissions/models/submission.js
+++ plugins/submissions/models/submission.js
@@ -1,5 +1,42 @@
 module.exports = function(Submission) {
 
+  var AWS = require('aws-sdk');
+  var s3 = new AWS.S3();
+  var params = {
+    Bucket: 'creative-challenge-photos',
+    Key: ''
+  };
+
+
   Submission.definition.properties.created.default = Date.now;
+
+  Submission.afterRemote('find', function(ctx, submissions, next){
+    submissions.forEach(function(submission){
+      params.Key = submission.url;
+      s3.getSignedUrl('getObject', params,
+        function (err, url) {
+          submission.url = url;
+      });
+    });
+    next();
+  });
+
+  Submission.afterRemote('findById', function(ctx, submission, next){
+    params.Key = submission.url;
+    s3.getSignedUrl('getObject', params,
+      function (err, url) {
+        submission.url = url;
+      });
+    next();
+  });
+
+  Submission.afterRemote('findOne', function(ctx, submission, next){
+    params.Key = submission.url;
+    s3.getSignedUrl('getObject', params,
+      function (err, url) {
+        submission.url = url;
+      });
+    next();
+  });
 
 };

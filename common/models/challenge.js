e4d81e4f059b0f8d1162b34f252a878883d0db49
Todd Bashor <jadedsurfer@gmail.com>
2015-07-24 16:34:32 -0700

    Add formattedSubmissionDueDate to challenge model

--- common/models/challenge.js
+++ common/models/challenge.js
@@ -1,5 +1,27 @@
 module.exports = function(Challenge) {
 
+  var moment = require('moment');
+
   Challenge.definition.properties.created.default = Date.now;
+
+  Challenge.afterRemote('find', function(ctx, challenges, next){
+    challenges.forEach(function(challenge){
+      challenge.formattedSubmissionDueDate =
+        moment(challenge.submissionDueDate).format('MMMM D, YYYY');
+    });
+    next();
+  });
+
+  Challenge.afterRemote('findById', function(ctx, challenge, next){
+    challenge.formattedSubmissionDueDate =
+      moment(challenge.submissionDueDate).format('MMMM D, YYYY');
+    next();
+  });
+
+  Challenge.afterRemote('findOne', function(ctx, challenge, next){
+    challenge.formattedSubmissionDueDate =
+      moment(challenge.submissionDueDate).format('MMMM D, YYYY');
+    next();
+  });
 
 };

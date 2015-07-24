ed7dbe49c092f0dfa4246884adc85e87e320e6b5
Todd Bashor <jadedsurfer@gmail.com>
2015-07-24 15:52:23 -0700

    #8 - Use s3upload capability in challenge module

--- ionic/www/js/challenge/challenge.controller.js
+++ ionic/www/js/challenge/challenge.controller.js
@@ -1,4 +1,5 @@
-module.exports = function($scope, $stateParams, ActiveChallenge) {
+module.exports = function(
+  $scope, $stateParams, ActiveChallenge, Submission, AppAuth) {
     if ($stateParams.challengeId) {
 
       $scope.challenge =
@@ -14,5 +15,27 @@ module.exports = function($scope, $stateParams, ActiveChallenge) {
       });
 
     }
+
+  AppAuth.ensureHasCurrentUser(function(user) {
+    if (user && user.email && user.id) {
+      $scope.currentUser =  user;
+    } else {
+      $scope.currentUser = {anonymous: true, id: null};
+    }
+  });
+
+  $scope.$on('fileUploaded', function(event, file){
+    if ($scope.currentUser.id !== null){
+      Submission.create({
+        userId: $scope.currentUser.id,
+        url: file.file.path_,
+        challengeId: $scope.challenge.id,
+        submitter: $scope.currentUser.email
+      }).$promise
+        .then(function(submission){
+          console.log(submission);
+        });
+    }
+  });
 
 };

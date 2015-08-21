4733d468b9398ee9376678368a9738e311431de6
Todd Bashor <jadedsurfer@gmail.com>
2015-08-21 12:01:12 -0700

    Sign submission urls when challenge is retrieved

--- common/models/challenge.js
+++ common/models/challenge.js
@@ -1,26 +1,74 @@
 module.exports = function(Challenge) {
 
   var moment = require('moment');
+  var AWS = require('aws-sdk');
+  var s3 = new AWS.S3();
+  var params = {
+    Bucket: 'creative-challenge-photos',
+    Key: ''
+  };
+
+  function assignUrl(submission){
+    return function(err, url){
+      submission.url = url;
+      return submission;
+    };
+  }
+
+  function processSubmissions(challenge){
+    var jsonChallenge = challenge.toJSON();
+      for(var i = 0; i < jsonChallenge.submissions.length; i++){
+        var currentSubmission = jsonChallenge.submissions[i];
+        params.Key = currentSubmission.url;
+        s3.getSignedUrl('getObject', params, assignUrl(currentSubmission));
+      }
+      return jsonChallenge;
+  }
+
+
 
   Challenge.definition.properties.created.default = Date.now;
 
   Challenge.afterRemote('find', function(ctx, challenges, next){
     challenges.forEach(function(challenge){
-      challenge.formattedSubmissionDueDate =
-        moment(challenge.submissionDueDate).format('MMMM D, YYYY');
+      try {
+        challenge.formattedSubmissionDueDate =
+          moment(challenge.submissionDueDate).format('MMMM D, YYYY');
+      } catch (err) {
+
+      }
     });
     next();
   });
 
   Challenge.afterRemote('findById', function(ctx, challenge, next){
-    challenge.formattedSubmissionDueDate =
-      moment(challenge.submissionDueDate).format('MMMM D, YYYY');
+    try {
+      challenge.formattedSubmissionDueDate =
+        moment(challenge.submissionDueDate).format('MMMM D, YYYY');
+      if (Array.isArray(challenge.submissions())){
+        ctx.result = processSubmissions(challenge);
+      }
+
+    } catch (err) {
+
+    }
+
+
     next();
   });
 
   Challenge.afterRemote('findOne', function(ctx, challenge, next){
-    challenge.formattedSubmissionDueDate =
-      moment(challenge.submissionDueDate).format('MMMM D, YYYY');
+    try {
+      challenge.formattedSubmissionDueDate =
+        moment(challenge.submissionDueDate).format('MMMM D, YYYY');
+      if (Array.isArray(challenge.submissions())){
+        ctx.result = processSubmissions(challenge);
+      }
+    } catch (err){
+
+    }
+
+
     next();
   });
 
